f0a5d3f0e8767d1090c02f07cde972557cd21a46
Ja90n <81578391+Ja90n@users.noreply.github.com>
2023-12-14 13:12:52 +0100

    Update BigJoystick.ino

--- BigJoystick.ino
+++ BigJoystick.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Mouse.h"
 
+const int xAxis = A0;
+const int yAxis = A1;
+
+void setup() {
+  Mouse.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int xReading = readAxis(A0);
+  int yReading = readAxis(A1);
+  
+  Mouse.move(xReading, yReading, 0);
 
+  delay(10);
+}
+
+int readAxis(int thisAxis) {
+  int reading = analogRead(thisAxis);
+
+  distance = Math.round(reading / 1023);
+
+  return distance;
 }
